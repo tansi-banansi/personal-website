a59fa2a6b8c3ea55b3930294838097cfc7e2c45f
Tanya <tanyatodorovaaa@gmail.com>
2024-09-02 15:00:41 +0100

    recent updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,10 +5,43 @@ const Project = () =>{
     return (
         <div id="project" className=" bg-neutral-200 dark:bg-neutral-700  text-neutral-800 dark:text-white  py-20" >
             
+            
             <div className="flex flex-col px-3 max-w-5xl mx-auto ">
                 <h2 className="h-5 border-b border-neutral-400 dark:border-neutral-100 text-2xl text-center">
                     <span className="bg-neutral-200 dark:bg-neutral-700 px-5">Projects</span>
                 </h2>
+
+                <div className="flex flex-col md:flex-row items-stretch gap-4 mt-14 mb-16 md:mb-0">
+
+                <div className="flex justify-center md:justify-start items-stretch">
+                    <img className="object-cover w:4/5 md:w-full h-52 " src="./files/e-comms-project.svg" alt='E-commerce website screenshot'/>
+                </div>
+
+                <div className=" w-full md:w-2/3 text-center md:text-start flex flex-col">
+                    <div className='my-4'>
+                        <h2 className='text-xl font-bold mb-2'>Serene (e-commerce)</h2>
+                        <p>An e-commerce website built for a beauty brand, incorporating Tailwind CSS to provide more design flexibility, while leveraging features like user authentication and a seamless checkout process.</p>
+                    </div>
+
+                    <hr className='border-1 border-neutral-500 mb-2'/>
+                    <div className='flex flex-row md:flex-col items-center md:items-start justify-between w-full'>
+                        <div className='flex flex-row mx-auto md:mx-0'>
+                            <p className='pr-3'>HTML</p>
+                            <p className='px-3'>CSS</p>
+                            <p className='px-3'>JS</p>
+                            <p className='px-3'>Figma</p>
+                            <p className='px-3'>React</p>
+                            <p className='px-3'>Tailwind</p>
+                        </div>
+                        <div className='md:mt-2'>
+                            <a href='#' target='_blank' rel="noreferrer" className='pointer-events-none' ><OpenInNewButton /></a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                </div>
 
                 <div className="flex flex-col-reverse md:flex-row items-stretch gap-4 mt-14">
 
